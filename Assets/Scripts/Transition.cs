de38812c571437d6b2b93d947d7dd4b47a49a89b
boi-one <91535051+boi-one@users.noreply.github.com>
2025-02-13 14:44:15 +0100

    transition effect

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    public static Transition reference;
+    Image transitionScreen;
+    float alpha = 0;
+    bool transitioning = false;
+    public Transform test;
+    public delegate void TransitionEvent();
+    public TransitionEvent transitionEvent = () => { };
+
+
+bool test2 = false;
+
+    void Awake()
+    {
+        reference = this;
+        transitionScreen = GetComponent<Image>();
+        CoverScreen();
+    }
+
+    void Update()
+    {
+        transitionScreen.color = new Vector4(0, 0, 0, alpha);
+        
+        TransitionFade();
+        if(alpha >= 1) test.position = new Vector3(test.position.x, 10);
+
+        if (Input.GetKeyDown(KeyCode.Space)) test2 = true;
+        if (test2) TransitionFade();
+    }
+
+    void CoverScreen()
+    {
+        if (!transitionScreen) return;
+        transitionScreen.transform.position = new Vector2(Screen.width / 2, Screen.height / 2);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    void FadeIn(float speed = 1f)
+    {
+        if (alpha < 1) alpha += 0.1f * (speed * Time.deltaTime);
+    }
+    
+    void FadeOut(float speed = 1f)
+    {
+        if (alpha > 0) alpha -= 0.1f * (speed * Time.deltaTime);
+    }
+
+    public void TransitionFade(float speed = 3f, float waitTime = 3f)
+    {
+        if(!transitioning) FadeIn(speed);
+        if (alpha >= 1 && !transitioning)
+        {
+            transitioning = true;
+            waitTime += Time.time;
+            transitionEvent();
+        }
+        if (Time.time > waitTime && transitioning)
+        {
+            FadeOut(speed);
+        }
+    }
+
+    void OnRectTransformDimensionsChange()
+    {
+        CoverScreen();
+    }
+
+}
