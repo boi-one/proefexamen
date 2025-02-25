5ed7caeed26598a7d6ddfb8603069f11cdd9f877
boi-one <91535051+boi-one@users.noreply.github.com>
2025-02-25 11:41:27 +0100

    saved changes

--- Assets/Scripts/Affliction.cs
+++ Assets/Scripts/Affliction.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-using UnityEngine;
-
-public class Affliction : MonoBehaviour
-{
-    Material material;
-    [SerializeField, Range(0, 1)]
-    float dirtyness = 1;
-    void Awake()
-    {
-        material = GetComponent<MeshRenderer>().material;
-    }
-
-    void Update()
-    {
-        material.SetFloat("_Dirtyness", Mathf.Clamp(dirtyness, 0, 1));
-    }
-}
-=======
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
@@ -49,4 +30,3 @@ public class Affliction
     } float _amount;
     public UnityEvent OnAdded, OnRemoved;
 }
->>>>>>> tools
