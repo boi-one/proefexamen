c4a7eee794ea929d4f2736f9a9d838c524725c20
Sil Wever <33390@ma-web.nl>
2025-02-26 11:02:05 +0100

    Update Assets/Scripts/CameraControl.cs
    sure
    
    Co-authored-by: tp <90894954+TeoPara@users.noreply.github.com>

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,9 +1,4 @@
-using System;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
 
 
 public class CameraControl : Interaction
