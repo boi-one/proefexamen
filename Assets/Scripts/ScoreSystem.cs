6efb92ca897d1b6875fc024d88b3855549464fcf
boi-one <91535051+boi-one@users.noreply.github.com>
2025-02-27 15:27:53 +0100

    put things together

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -24,8 +24,8 @@ public class ScoreSystem : MonoBehaviour
     float _scoreTimer = 100;
     float progress;
     
-    [SerializeField]
-    List<Transform> teeth = new();
+    //[SerializeField]
+    //List<Transform> teeth = new();
     Text scoreText => _scoreText ??= GetComponentInChildren<Text>();
     Text _scoreText;
     Slider progressBar => _progressBar ??= GetComponentInChildren<Slider>();
@@ -37,7 +37,7 @@ public class ScoreSystem : MonoBehaviour
 
     void ScoreManager()
     {
-        progress = teeth.Count(_ => _.GetComponent<Tooth>().clean) / (float)teeth.Count;
+        //progress = teeth.Count(_ => _.GetComponent<Tooth>().clean) / (float)teeth.Count;
         scoreText.text = scoreTimer > 0 ? ((int)(difficultyMultiplier * scoreTimer)).ToString() : invokeNoTimeLeft();
         progressBar.value = progress;
         new Action(progress == 1 ? (Action)(() => Win.Invoke()) : () => { }).Invoke();
--- Assets/Scripts/Tooth.cs
+++ Assets/Scripts/Tooth.cs
@@ -9,33 +9,37 @@ using UnityEngine;
 public class Tooth : Part
 {
     public bool clean;
-    // MeshRenderer meshRenderer;
-    //
-    // public void Awake()
-    // {
-    //     List<Material> toothAfflicions = new List<Material>();
-    //     foreach (var affliction in Afflictions)
-    //     {
-    //         toothAfflicions.Add(affliction.material);
-    //         affliction.Amount = UnityEngine.Random.Range(0.0f, 1.0f);
-    //     }
-    //     transform.GetChild(0).GetComponent<MeshRenderer>().SetMaterials(toothAfflicions);
-    //     meshRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
-    // }
-    //
-    // void Update()
-    // {
-    //
-    //     for (int i = 0; i < Afflictions.Length; i++)
-    //     {
-    //         meshRenderer.materials[i].SetFloat("_Dirtyness", Afflictions[i].Amount);
-    //     }
-    // }
-    //
-    // void Clean(AfflictionType toolType, float decreaseAmount)
-    // {
-    //     if (Afflictions.First(_ => _.Type == toolType) is { } affliction)
-    //         affliction.Amount -= decreaseAmount;
-    // }
+    MeshRenderer meshRenderer;
+
+    public void Awake()
+    {
+        meshRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+        List<Material> toothAfflicions = new List<Material>();
+        foreach (var affliction in Afflictions)
+        {
+            toothAfflicions.Add(meshRenderer.materials[0]);
+            toothAfflicions.Add(affliction.material);
+            affliction.Amount = UnityEngine.Random.Range(0.0f, 1.0f);
+        }
+        meshRenderer.SetMaterials(toothAfflicions);
+    }
+
+    void Update()
+    {
+
+        for (int i = 0; i < Afflictions.Length; i++)
+        {
+            meshRenderer.materials[i].SetFloat("_Dirtyness", Afflictions[i].Amount);
+        }
+    }
+
+    void Clean(AfflictionType toolType, float decreaseAmount)
+    {
+        if (Afflictions.First(_ => _.Type == toolType) is { } affliction)
+        {
+            affliction.Amount -= decreaseAmount;
+            if (affliction.Amount == 0) clean = true;
+        }
+    }
 }
 
