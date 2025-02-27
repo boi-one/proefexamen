43baacf195ab864335400f85dc14d77ba29a380a
Masenkyo <33390@ma-web.nl>
2025-02-27 14:48:59 +0100

    gl it's your turn

--- Assets/Scripts/Tooth.cs
+++ Assets/Scripts/Tooth.cs
@@ -9,33 +9,33 @@ using UnityEngine;
 public class Tooth : Part
 {
     public bool clean;
-    MeshRenderer meshRenderer;
-
-    public void Awake()
-    {
-        List<Material> toothAfflicions = new List<Material>();
-        foreach (var affliction in Afflictions)
-        {
-            toothAfflicions.Add(affliction.material);
-            affliction.Amount = UnityEngine.Random.Range(0.0f, 1.0f);
-        }
-        transform.GetChild(0).GetComponent<MeshRenderer>().SetMaterials(toothAfflicions);
-        meshRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
-    }
-
-    private void Update()
-    {
-
-        for (int i = 0; i < Afflictions.Length; i++)
-        {
-            meshRenderer.materials[i].SetFloat("_Dirtyness", Afflictions[i].Amount);
-        }
-    }
-
-    void Clean(AfflictionType toolType, float decreaseAmount)
-    {
-        if (Afflictions.First(_ => _.Type == toolType) is { } affliction)
-            affliction.Amount -= decreaseAmount;
-    }
+    // MeshRenderer meshRenderer;
+    //
+    // public void Awake()
+    // {
+    //     List<Material> toothAfflicions = new List<Material>();
+    //     foreach (var affliction in Afflictions)
+    //     {
+    //         toothAfflicions.Add(affliction.material);
+    //         affliction.Amount = UnityEngine.Random.Range(0.0f, 1.0f);
+    //     }
+    //     transform.GetChild(0).GetComponent<MeshRenderer>().SetMaterials(toothAfflicions);
+    //     meshRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+    // }
+    //
+    // void Update()
+    // {
+    //
+    //     for (int i = 0; i < Afflictions.Length; i++)
+    //     {
+    //         meshRenderer.materials[i].SetFloat("_Dirtyness", Afflictions[i].Amount);
+    //     }
+    // }
+    //
+    // void Clean(AfflictionType toolType, float decreaseAmount)
+    // {
+    //     if (Afflictions.First(_ => _.Type == toolType) is { } affliction)
+    //         affliction.Amount -= decreaseAmount;
+    // }
 }
 
