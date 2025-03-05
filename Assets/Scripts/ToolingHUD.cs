ba6329dfc061c6a103158e0e641b214b1dddfb0f
Teo <90894954+TeoPara@users.noreply.github.com>
2025-03-05 10:00:26 +0100

    WIP

--- Assets/Scripts/ToolingHUD.cs
+++ Assets/Scripts/ToolingHUD.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 public class ToolingHUD : MonoBehaviour
 {
     [SerializeField] Transform slotPrefab;
-    
+
+    Canvas canvas;
     void Awake()
     {
         foreach (Tool tool in FindObjectsByType<Tool>(FindObjectsSortMode.None))
@@ -13,7 +14,17 @@ public class ToolingHUD : MonoBehaviour
             var created = Object.Instantiate(slotPrefab.gameObject, slotPrefab.parent);
             created.SetActive(true);
             created.transform.GetChild(0).GetComponent<Image>().sprite = tool.UiIcon;
-            created.GetComponent<Button>().onClick.AddListener(() => { Tool.CurrentlySelectedTool = tool; });
+            created.GetComponent<Button>().onClick.AddListener(() => 
+            {
+                Tool.CurrentlySelectedTool = tool;
+            });
         }
+    }
+    
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        canvas.enabled = true;
     }
 }
